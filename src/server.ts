29db0f893760ca7a767b50a00d71a86f272f27dd
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-04-14 11:45:15 -0300

    Configurando projeto

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ const app = express();
 
 app.get('/', (req, res) => {
     return res.status(200).json({ message: 'Hello Dev de 100mil'})
-})
+});
 
 app.listen(3333, () => {
     console.log('🚀 Server started on port 3333')
-})
+});
